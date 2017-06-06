2393522b3a9448c99c718be63511eec9c9bffcc0
Thomas Waters <Thomas.waters05@gmail.com>
2017-06-05 18:31:12 -0700

    materialize working

--- RackerApplication/App_Start/BundleConfig.cs
+++ RackerApplication/App_Start/BundleConfig.cs
@@ -16,19 +16,18 @@ namespace RackerApplication
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/materialize.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
                       "~/Scripts/angular.js",
                       "~/Scripts/angular-route.js",
                       "~/app/app.js")
                       .IncludeDirectory("~/app", "*.js", true));
+
+            bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
+                     "~/Content/js/materialize.js"));
         }
     }
 }
